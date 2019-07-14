4dba7c0f6c726ea00c91b5c1688869b4a4722e5f
Alex Gerasimyuk <2011.sanya@gmail.com>
2019-07-14 13:59:15 +0300

    events and favicons

--- dist/js/common.js
+++ dist/js/common.js
@@ -8,4 +8,18 @@ $(document).ready(function() {
 		$( ".header__menu" ).removeClass( "active" );
 		$( ".menu-overlay" ).removeClass( "active" );
 	});
+
+	//tabs
+	$('.events-tab-link a').click(function(event){
+		event.preventDefault();
+		$('.events-tab-link a').removeClass('active');
+		$(this).addClass('active');
+		$('.events-tab').hide();
+		$($(this).attr('href')).show();
+	});
+
+	//drop menu
+	$( ".event-sidebar__drop" ).click(function() {
+		$( this ).next().slideToggle( "fast" );
+	});
 });
